06ae80856931469067c831f3380bd8d0a00d7380
danocmx <glencocomaster@centrum.cz>
2025-01-09 22:18:50 +0100

    fix: remove unused parameter from snapshot

--- src/params/snapshot.ts
+++ src/params/snapshot.ts
@@ -1,12 +1,10 @@
 export type GetSnapshotParams = {
   item: string;
-  skip?: number;
 };
 
 export function constructGetSnapshotParams(params: GetSnapshotParams) {
   return {
     sku: params.item,
-    skip: params.skip || 0,
     appid: 440,
   };
 }
